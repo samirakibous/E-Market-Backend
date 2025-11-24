00cb0ca653bb71ab58464acf25cc2586f2909069
LatrachDev <m.latrach.youcode@gmail.com>
2025-11-24 09:41:41 +0100

    change the response to send all user data except password | make the profile accessible to all users

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,6 +3,7 @@ import { register, login, logout } from '../controllers/authController.js';
 import { isAuthenticated, isAdmin } from '../middlewares/auth.js';
 import validate from '../middlewares/validate.js';
 import { userSchema } from '../validations/userSchema.js';
+import User from '../models/User.js';
 const router = express.Router();
 
 const loginSchema = userSchema.pick(['email', 'password']);
@@ -16,8 +17,21 @@ router.post('/logout', isAuthenticated, logout);
 // Example of protected route
 // Example of protected route
 // Example of protected route
-router.get('/profile', isAuthenticated, (req, res) => {
-  res.json({ message: `Welcome, user ${req.user.id}`, user: req.user });
+router.get('/profile', isAuthenticated, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json({
+      message: `Welcome, user ${user.fullname}`,
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 export default router;
